a8ef4801e2bc99effbb253741caeaf33271f7de6
zhrgithub <zhr_java@163.com>
2022-07-24 12:21:49 +0800

    添加网关设置

---
+++ rear/zuul/src/main/java/com/spaceobj/zuul/SpaceobjZuulApplication.java
@@ -0,0 +1,47 @@
+package com.spaceobj.zuul;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpaceobjZuulApplication {
+
+
+    @RestController
+    public class NacosController{
+
+        @Autowired
+        private LoadBalancerClient loadBalancerClient;
+        @Autowired
+        private RestTemplate restTemplate;
+
+        @Value("${spring.application.name}")
+        private String appName;
+
+        @GetMapping("/echo/app-name")
+        public String echoAppName(){
+            //使用 LoadBalanceClient 和 RestTemplate 结合的方式来访问
+            ServiceInstance serviceInstance = loadBalancerClient.choose("spaceobj-advertise");
+            String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
+            System.out.println("request url:"+url);
+            return restTemplate.getForObject(url,String.class);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpaceobjZuulApplication.class, args);
+    }
+
+}
---
+++ rear/zuul/src/main/java/com/spaceobj/zuul/config/RestComplateConfig.java
@@ -0,0 +1,21 @@
+package com.spaceobj.zuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zhr_java@163.com
+ * @date 2022/7/17 22:10
+ */
+@Configuration
+public class RestComplateConfig {
+
+    //实例化 RestTemplate 实例
+    @Bean
+    public RestTemplate restTemplate() {
+
+        return new RestTemplate();
+    }
+
+}
