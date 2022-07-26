693f9b80cce4af42dc52c28248913cecaf8901a0
zhrgithub <zhr_java@163.com>
2022-07-27 00:44:37 +0800

    增加用户管理、认证、路由鉴权，通过satoken实现单点登录

--- rear/user/src/main/java/com/spaceobj/user/service/impl/PhotoServiceImpl.java
+++ rear/user/src/main/java/com/spaceobj/user/service/impl/PhotoServiceImpl.java
@@ -5,11 +5,13 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.spaceobj.user.mapper.SysPhotoMapper;
 import com.spaceobj.user.pojo.SysPhoto;
 import com.spaceobj.user.service.PhotoService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author zhr_java@163.com
  * @date 2022/7/27 00:28
  */
+@Service
 public class PhotoServiceImpl extends ServiceImpl<SysPhotoMapper, SysPhoto> implements PhotoService {
 
     @Override
