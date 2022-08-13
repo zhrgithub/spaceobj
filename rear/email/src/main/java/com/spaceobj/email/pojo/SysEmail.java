3331211d7d440138a68d14cccad3a8ddfce021ff
zhrgithub <zhr_java@163.com>
2022-08-14 00:37:01 +0800

    实体类添加有参构造器 无参构造器

--- rear/email/src/main/java/com/spaceobj/email/pojo/SysEmail.java
+++ rear/email/src/main/java/com/spaceobj/email/pojo/SysEmail.java
@@ -3,7 +3,9 @@ package com.spaceobj.email.pojo;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotBlank;
 
 import java.io.Serializable;
@@ -14,6 +16,8 @@ import java.io.Serializable;
  */
 @Data
 @TableName(value = "sys_email")
+@AllArgsConstructor
+@NoArgsConstructor
 public class SysEmail implements Serializable {
 
   @TableId(value = "email_id")
