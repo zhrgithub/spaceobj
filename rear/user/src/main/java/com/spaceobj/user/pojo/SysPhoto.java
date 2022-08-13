2644fa98e70581c6f614ea48e471915fde9b1e04
zhrgithub <zhr_java@163.com>
2022-08-14 00:35:58 +0800

    实体类添加有参构造器 无参构造器

--- rear/user/src/main/java/com/spaceobj/user/pojo/SysPhoto.java
+++ rear/user/src/main/java/com/spaceobj/user/pojo/SysPhoto.java
@@ -3,8 +3,10 @@ package com.spaceobj.user.pojo;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -14,6 +16,8 @@ import java.io.Serializable;
 @Data
 @TableName(value = "sys_photo")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SysPhoto implements Serializable {
 
   @TableId(value = "user_id")
