8b3f0c1ecf3aaf282d1cd44a852eb841688441aa
zhrgithub <zhr_java@163.com>
2022-09-29 10:17:16 +0800

    生成rsa的工具类

---
+++ rear/user/src/main/java/com/spaceobj/user/utils/GenerateRsa.java
@@ -0,0 +1,32 @@
+package com.spaceobj.user.utils;
+
+import cn.dev33.satoken.secure.SaSecureUtil;
+
+import java.util.HashMap;
+
+/**
+ * 公钥秘钥生成器
+ *
+ * @author zhr_java@163.com
+ * @date 2022/9/16 20:32
+ */
+public class GenerateRsa {
+
+  /**
+   * 初始化公钥秘钥
+   *
+   * @return
+   */
+  public static HashMap<String, String> rsaGenerateKeyPair() {
+    HashMap<String, String> rsa;
+    try {
+      rsa = SaSecureUtil.rsaGenerateKeyPair();
+    } catch (Exception e) {
+      e.printStackTrace();
+      rsa = new HashMap<>();
+      rsa.put("msg", "rsa初始化失败");
+      return rsa;
+    }
+    return rsa;
+  }
+}
