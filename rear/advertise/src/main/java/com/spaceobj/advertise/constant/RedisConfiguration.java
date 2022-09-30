49a51f3dc5a4b65207eaab2814951f33f4e0ed31
zhrgithub <zhr_java@163.com>
2022-09-30 13:21:39 +0800

    解决Redis序列化和反序列化包名路径不一致的问题

--- rear/advertise/src/main/java/com/spaceobj/advertise/constant/RedisConfiguration.java
+++ rear/advertise/src/main/java/com/spaceobj/advertise/constant/RedisConfiguration.java
@@ -27,7 +27,7 @@ public class RedisConfiguration extends CachingConfigurerSupport {
 
     // 使用StringRedisSerializer来序列化和反序列化redis的key值
     template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(serializer);
+      template.setValueSerializer(serializer);
 
     // Hash的key也采用StringRedisSerializer的序列化方式
     template.setHashKeySerializer(new StringRedisSerializer());
