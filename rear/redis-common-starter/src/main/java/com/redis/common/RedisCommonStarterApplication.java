3b45207edd9350951010d1c5da714a67b239e6f2
zhrgithub <zhr_java@163.com>
2022-10-06 13:43:09 +0800

    整理依赖文件，删除无用的import

--- rear/redis-common-starter/src/main/java/com/redis/common/RedisCommonStarterApplication.java
+++ rear/redis-common-starter/src/main/java/com/redis/common/RedisCommonStarterApplication.java
@@ -1,9 +1,11 @@
 package com.redis.common;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhr
+ */
 @SpringBootApplication
 public class RedisCommonStarterApplication {
 
