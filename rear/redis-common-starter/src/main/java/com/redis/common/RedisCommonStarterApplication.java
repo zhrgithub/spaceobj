c0710caab618f010d4785af3d9095bf97bf657cc
zhrgithub <zhr_java@163.com>
2022-10-06 11:43:32 +0800

    自定义Redis-common-starter

---
+++ rear/redis-common-starter/src/main/java/com/redis/common/RedisCommonStarterApplication.java
@@ -0,0 +1,15 @@
+package com.redis.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCommonStarterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RedisCommonStarterApplication.class, args);
+    }
+
+}
