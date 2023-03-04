5e01e6e76c6344bdc7b7da763d85f6f6696b2d20
zhrgithub <zhr_java@163.com>
2023-03-04 21:23:35 +0800

    代码整理

--- common/common-redis/src/main/java/com/spaceobj/common/redis/config/RedissonConfiguration.java
+++ common/common-redis/src/main/java/com/spaceobj/common/redis/config/RedissonConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ObjectUtils;
 
 /**
  * @author zhr_java@163.com
